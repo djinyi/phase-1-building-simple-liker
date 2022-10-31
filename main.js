bb247baa531ba38a49f642a6fad4d2fa28e25571
Daisy Yi <daisyyi@rocketmail.com>
2022-10-31 15:01:21 -0700

    commit tree 101442b806e9bacf5bbe4be6b11cebeed3ac8134

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", () => {
+  const errorModal = document.querySelector("#modal")
+  errorModal.classList.add("hidden")
+})
 
 
 
